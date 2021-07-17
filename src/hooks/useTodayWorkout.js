bbaa8212a2fc93f81439c8fa5ab3ade022ba4602
Kim Hojeong <hojeong0713@gmail.com>
2021-07-17 16:41:42 +0200

    Add new page

---
+++ src/hooks/useTodayWorkout.js
@@ -0,0 +1,320 @@
+const chloeTingTwoWeeksSchedule = [
+  { day: 0, contents: "IT'S A REST DAY!!" },
+  {
+    day: 1,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body workout",
+        youtube: "https://youtu.be/Crda1Sv9mks",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "legs & glutes",
+        youtube: "https://youtu.be/PrRResjXk6Q",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 2,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body workout",
+        youtube: "https://youtu.be/Crda1Sv9mks",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "upper body workout",
+        youtube: "https://youtu.be/VlkL3kg2Nno",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 3,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body workout",
+        youtube: "https://youtu.be/Crda1Sv9mks",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "lower body workout",
+        youtube: "https://youtu.be/_Y2k5O6Puik",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 4,
+    contents: "IT'S A REST DAY!!",
+  },
+  {
+    day: 5,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body workout",
+        youtube: "https://youtu.be/Crda1Sv9mks",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "upper body workout",
+        youtube: "https://youtu.be/VlkL3kg2Nno",
+      },
+      {
+        title: "plank challenge",
+        youtube: "https://youtu.be/7EHGO-alrwI",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 6,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body workout",
+        youtube: "https://youtu.be/Crda1Sv9mks",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "lower body workout",
+        youtube: "https://youtu.be/_Y2k5O6Puik",
+      },
+      {
+        title: "hip dips",
+        youtube: "https://youtu.be/umUtH9gZIfc",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 7,
+    contents: "IT'S A REST DAY!!",
+  },
+  {
+    day: 8,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body burn",
+        youtube: "https://youtu.be/Sc-2yZaKsBU",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "upper body workout",
+        youtube: "https://youtu.be/VlkL3kg2Nno",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 9,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body workout",
+        youtube: "https://youtu.be/Crda1Sv9mks",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "lower body workout",
+        youtube: "https://youtu.be/_Y2k5O6Puik",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 10,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body burn",
+        youtube: "https://youtu.be/Sc-2yZaKsBU",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "plank challenge",
+        youtube: "https://youtu.be/7EHGO-alrwI",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 11,
+    contents: "IT'S A REST DAY!!",
+  },
+  {
+    day: 12,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body burn",
+        youtube: "https://youtu.be/Sc-2yZaKsBU",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "lower body workout",
+        youtube: "https://youtu.be/_Y2k5O6Puik",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 13,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body workout",
+        youtube: "https://youtu.be/Crda1Sv9mks",
+      },
+      {
+        title: "upper body workout",
+        youtube: "https://youtu.be/VlkL3kg2Nno",
+      },
+      {
+        title: "lower abs",
+        youtube: "https://youtu.be/ZveSMkUr0l0",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+  {
+    day: 14,
+    contents: [
+      {
+        title: "warm up",
+        youtube: "https://youtu.be/-p0PA9Zt8zk",
+      },
+      {
+        title: "full body burn",
+        youtube: "https://youtu.be/Sc-2yZaKsBU",
+      },
+      {
+        title: "abs workout",
+        youtube: "https://youtu.be/h8ctkfSx6R0",
+      },
+      {
+        title: "6 pack abs",
+        youtube: "https://youtu.be/SX6Vhj9wZh8",
+      },
+      {
+        title: "lower body workout",
+        youtube: "https://youtu.be/_Y2k5O6Puik",
+      },
+      {
+        title: "cooldown",
+        youtube: "https://youtu.be/cO2RqgslEjk",
+      },
+    ],
+  },
+];
+
+const useTodayWorkout = () => {
+  const startedDate = new Date("2021-07-13");
+
+  const whichWorkoutShoudIDo = (startedDate, schedule) => {
+    const date = new Date();
+    const todayLesson =
+      (Math.floor((date - startedDate) / (60 * 60 * 24 * 1000)) + 1) % 16;
+    const index = schedule.findIndex((lesson) => lesson.day === todayLesson);
+    return schedule[index];
+  };
+
+  const workout = whichWorkoutShoudIDo(startedDate, chloeTingTwoWeeksSchedule);
+
+  return [workout];
+};
+
+export default useTodayWorkout;
